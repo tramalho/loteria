6f51f96512453777e4693879faa7e3ee664ce37b
tramalho <thiagofacul@gmail.com>
2020-05-21 23:14:20 -0300

    34. Aplicativo Loteria: Preparando as telas

--- Loteria/ViewController.swift
+++ Loteria/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbGameType: UILabel!
+    @IBOutlet weak var scGameType: UISegmentedControl!
+    @IBOutlet var balls: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func generateGame() {
+    }
 }
 
