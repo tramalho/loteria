14155f5e9d1ccf78cdfb32d93fe1568cd9609518
tramalho <thiagofacul@gmail.com>
2020-05-22 22:07:35 -0300

    35. Finalizando Aplicativo Loteria

--- Loteria/ViewController.swift
+++ Loteria/ViewController.swift
@@ -1,26 +1,67 @@
-//
-//  ViewController.swift
-//  Loteria
-//
-//  Created by Thiago Antonio Ramalho on 21/05/20.
-//  Copyright © 2020 Tramalho. All rights reserved.
-//
-
-import UIKit
-
-class ViewController: UIViewController {
-
-    @IBOutlet weak var lbGameType: UILabel!
-    @IBOutlet weak var scGameType: UISegmentedControl!
-    @IBOutlet var balls: [UIButton]!
+    //
+    //  ViewController.swift
+    //  Loteria
+    //
+    //  Created by Thiago Antonio Ramalho on 21/05/20.
+    //  Copyright © 2020 Tramalho. All rights reserved.
+    //
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    import UIKit
+    
+    enum GameType: String {
+        case megasena = "Mega-Sena"
+        case quina = "Quina"
     }
-
-
-    @IBAction func generateGame() {
+    
+    infix operator >-<
+    func >-< (total:Int, universe: Int) -> [Int] {
+        var result:[Int] = []
+        
+        while result.count < total {
+            let randomNumber = Int(arc4random_uniform(UInt32(universe))+1)
+            if !result.contains(randomNumber) {
+                result.append(randomNumber)
+            }
+        }
+        
+        return result.sorted()
     }
-}
-
+    
+    class ViewController: UIViewController {
+        
+        @IBOutlet weak var lbGameType: UILabel!
+        @IBOutlet weak var scGameType: UISegmentedControl!
+        @IBOutlet var balls: [UIButton]!
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            showNumbers(for: .megasena)
+        }
+        
+        func showNumbers(for type:GameType)  {
+            lbGameType.text = type.rawValue
+            var game: [Int] = []
+            switch type {
+            case .megasena:
+                game = 6>-<60
+                balls.last?.isHidden = false
+            case .quina:
+                game = 5>-<80
+                balls.last?.isHidden = true
+            }
+            
+            for (index, value) in game.enumerated() {
+                balls[index].setTitle("\(value)", for: .normal)
+            }
+        }
+        
+        @IBAction func generateGame() {
+            switch scGameType.selectedSegmentIndex {
+            case 0:
+                showNumbers(for: .megasena)
+            default:
+                showNumbers(for: .quina)
+            }
+        }
+    }
+    
